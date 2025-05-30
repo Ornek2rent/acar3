533a8ddf1f4ad0f45ddba3e6f8c30bfc749703f4
Ornek2rent <ornek2rent@gmail.com>
2025-05-30 15:21:51 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 
 const CONFIG = {
   FORM_ENDPOINT: 'https://script.google.com/macros/s/AKfycbz3OlzaKfk01bSI8FxbeLkwAytohxSgun_b7bxCEBQdJ4NKpbUCrlKcg1VAASsfHg6q/exec',
-  CARS_API_URL: 'https://script.google.com/macros/s/AKfycbwKEUS71H3L6p6Rx1yHfDppOFyNuzLiD4sMWCM11mviY6yiVlNnKzJ0P-HupKsPHqH-/exec'
+  CARS_API_URL: 'https://script.google.com/macros/s/AKfycbyxP93Jet9z1ah4da7cjXB6Vag7-sPGNKMe29eDmEZRn_RVAFqhSI58smm7l3B-BQKk/exec'
 };
